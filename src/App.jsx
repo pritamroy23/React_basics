0c1cddf83236035b35ad6c783feaf08ef9381cd8
Pritam Roy <roypritam3114@gmail.com>
2025-07-29 21:23:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-
-function App() {
-  return "hellow"
+import React from 'react'
+const App = () => {
+  const user = "pritam";
+  const abc =() => {
+    console.log("button clicked");
+  }
+  const age = 20;
+  return (
+    <div >
+      <h1>Hello {user}, your age {age}</h1>
+      <button onClick={abc}>change user</button>
+      
+    </div>
+  );
 
 }
 export default App;
