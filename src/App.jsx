53fc4b859772fdc839c223a5d8945fe7377d6735
Pritam Roy <roypritam3114@gmail.com>
2025-07-29 22:56:22 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div >
       <h1>Hello {user}, your age {age}</h1>
-      <button onClick={abc}>change user</button>
+      <button className ="btn" onClick={abc}>change user</button>
       
     </div>
   );
