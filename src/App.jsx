1e3e98d6ff31912584001d4d9d682389637a97e1
Pritam Roy <roypritam3114@gmail.com>
2025-07-30 21:45:50 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const App = () => {
   const [a, setA] = React.useState(10)
- 
+  const changeA = () => {
+    setA(20);
+  }
   return (
     <div> 
       <h1>value of a is {a} </h1>
-      <button>changeA</button>
+      <button onClick={changeA}>change A</button>
     </div>
   )
 }
